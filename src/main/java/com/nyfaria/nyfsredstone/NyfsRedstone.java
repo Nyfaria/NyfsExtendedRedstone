736c67683492fd416892a1b28f55deeaab2cc004
Nyfaria <kinaria3324@gmail.com>
2022-01-01 11:20:07 -0600

    initial

---
+++ src/main/java/com/nyfaria/nyfsredstone/NyfsRedstone.java
@@ -0,0 +1,13 @@
+package com.nyfaria.nyfsredstone;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NyfsRedstone implements ModInitializer {
+
+	public static final String MOD_ID = "nyfsredstone";
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
